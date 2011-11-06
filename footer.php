036e0ec993fb7c04524cb94994ccf199bc593fe9
Koji Jimba <dj.jimba@gmail.cm>
2011-11-07 00:50:37 +0900

    adding the files

---
+++ footer.php
@@ -0,0 +1,19 @@
+                    <br class="clear">
+               </div><!-- /#contents -->
+              
+<!-- Footer -->              
+               <div id="footer">
+              
+                    <?php dynamic_sidebar( 'footer-widget' ); ?>
+                   
+                    <p class="copy">
+                         &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.
+                    </p>
+
+               </div><!-- /#footer -->
+          </div><!-- /#wrapper -->
+     </div><!-- /#page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<div id="side">
+    <div class="widget-area">
+        <ul>
+            <?php dynamic_sidebar( 'side-widget' ); ?>
+        </ul>
+    </div><!-- /.widget-area -->
+ </div><!-- /#side -->
