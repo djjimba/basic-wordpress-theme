bc433bde124e8922118093304fa1ae77574db1c3
Koji Jimba <dj.jimba@gmail.cm>
2011-11-07 01:00:43 +0900

    不要なファイルを削除

--- sidebar.php
+++
@@ -1,7 +0,0 @@
-<div id="side">
-    <div class="widget-area">
-        <ul>
-            <?php dynamic_sidebar( 'side-widget' ); ?>
-        </ul>
-    </div><!-- /.widget-area -->
- </div><!-- /#side -->
